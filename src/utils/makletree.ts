8dc2b48c9f1177af6935f5b5aa13188f6d6b59fe
batublockdev <batublockchain@gmail.com>
2025-07-02 09:31:07 -0500

    MakleTreeBuilder done

--- src/utils/makletree.ts
+++ src/utils/makletree.ts
@@ -1,130 +1,212 @@
 
-import * as circomlibjs from 'circomlibjs';
-let current: number;
-async function hash(x: number, y: number): Promise<number> {
-    const poseidon: circomlibjs.Poseidon = await circomlibjs.buildPoseidon();
-    current = Number(poseidon([x, y]));
-    return current;
+import * as circomlibjs from "circomlibjs";
+
+let poseidon: any;
+
+async function initPoseidon() {
+    poseidon = await circomlibjs.buildPoseidon(); // remove "const"
+
 }
-let data = [5, 6, 22, 44, 66, 88]
-let tree: number[][] = Array.from({ length: 20 }, () => []);
-data.length + 1;
-let currentIndex = 0;
-let mine = [];
-let left;
-let right;
-let index = 4;
-let z = data.length;
-let leavesToExpect;
-// operar [0-1] [2-3] [4-5] [6-0] then we have zero this is the level 0
-// but we need to tack [4 - 5] then save it in mine
-for (let i = 0; i < 20; i++) {
-    if (z % 2 == 0) {
-        z++
-        leavesToExpect = (z) / 2;
-    } else {
-        leavesToExpect = (z) / 2;
+
+function hash(a: bigint, b: bigint): bigint {
+    const rawHash = poseidon([a, b]); // poseidon output is field element
+    const result = poseidon.F.toObject(rawHash) as bigint; // convert to bigint
+    return result;
+}
+
+function buildMerkleTree(leaves: bigint[]): [bigint[][], number] {
+    const tree: bigint[][] = [];
+
+    tree[0] = leaves; // Level 0: hashed leaves
+
+    let currentLevel = tree[0];
+    let level = 0;
+    while (currentLevel.length > 1) {
+
+        const nextLevel: bigint[] = [];
+        for (let i = 0; i < currentLevel.length; i += 2) {
+            const left = currentLevel[i];
+            console.log("left", left);
+            const right = currentLevel[i + 1] ?? zeros(level); // duplicate last if odd
+            console.log("right", right);
+            nextLevel.push(hash(left, right));
+            console.log("/n");
+            console.log("hash", nextLevel[nextLevel.length - 1]);
+        }
+        tree.push(nextLevel);
+        level++;
+        currentLevel = nextLevel;
     }
 
-    for (let j = 0; j < leavesToExpect; j++) {
-        tree[i][j] = await hash(data[j], data[j + 1]);
-    }
-    if (currentIndex % 2 == 0) {
-        left = data[index];
-        right = zeros(i);
-    } else {
-        left = _proposalTrees[proposalId].filledSubtrees[i];
-        right = currentLevelHash;
-    }
-    currentLevelHash = hashLeftRight(left, right);
-    currentIndex /= 2;
+    return [tree, level];
 }
+
+function getMerkleProof(tree: bigint[][], index: number) {
+    const proof: { sibling: bigint; isLeft: boolean }[] = [];
+
+    for (let level = 0; level < tree.length - 1; level++) {
+
+        const isLeft = index % 2 === 0;
+
+        const siblingIndex = isLeft ? index + 1 : index - 1;
+
+        const levelNodes = tree[level];
+        const siblingHash = levelNodes[siblingIndex] ?? (isLeft ? zeros(level) : levelNodes[index]); // if missing, duplicate
+
+        proof.push({
+            sibling: siblingHash,
+            isLeft: !isLeft,
+        });
+
+        index = Math.floor(index / 2);
+    }
+
+    return proof;
+}
+function normalizeProof(proof: { sibling: bigint; isLeft: boolean }[], root: bigint, levels: number) {
+    const pathElements: string[] = [];
+    const pathIndices: number[] = [];
+
+    for (const { sibling, isLeft } of proof) {
+        let siblingBigInt: bigint;
+
+        siblingBigInt = sibling;
+
+
+        pathElements.push(siblingBigInt.toString());
+        pathIndices.push(isLeft ? 1 : 0);
+    }
+    let current = root;
+    for (let i = levels; i < 20; i++) {
+        current = hash(current, zeros(i));
+        pathElements.push(zeros(i).toString());
+        pathIndices.push(0);
+    }
+
+
+    return { pathElements, pathIndices, current };
+}
+
+
 function zeros(i: number): bigint {
     if (i == 0) {
         return BigInt(
-            5365074742933140195701460912837558111746482509126416785866028967095537070591
+            "5365074742933140195701460912837558111746482509126416785866028967095537070591"
         );
     } else if (i == 1) {
         return BigInt(
-            18927995262277541656172313965917959133749249173481326288359404073453342459520
+            "18927995262277541656172313965917959133749249173481326288359404073453342459520"
         );
     } else if (i == 2) {
         return BigInt(
-            16731048699270784421706570066001991137631760108429146124137121216236553054391
+            "16731048699270784421706570066001991137631760108429146124137121216236553054391"
         );
     } else if (i == 3) {
         return BigInt(
-            16538499595934659613727544616962797785338492501618747306146816584688577735208
+            "16538499595934659613727544616962797785338492501618747306146816584688577735208"
         );
     } else if (i == 4) {
         return BigInt(
-            9080356992685742511681529878226919830400349732756896631827555189762739734491
+            "9080356992685742511681529878226919830400349732756896631827555189762739734491"
         );
     } else if (i == 5) {
         return BigInt(
-            5424144409132066577541240512439621178657103249409002506486103738618538352024
+            "5424144409132066577541240512439621178657103249409002506486103738618538352024"
         );
     } else if (i == 6) {
         return BigInt(
-            10544296693406531387206535330554893730829540929428087193592756592216676445216
+            "10544296693406531387206535330554893730829540929428087193592756592216676445216"
         );
     } else if (i == 7) {
         return BigInt(
-            7564988964993902948097824678805225006987812851830016626680599597402005451957
+            "7564988964993902948097824678805225006987812851830016626680599597402005451957"
         );
     } else if (i == 8) {
         return BigInt(
-            12169719382686741916580483745316623687880402026431235449348059358607961733224
+            "12169719382686741916580483745316623687880402026431235449348059358607961733224"
         );
     } else if (i == 9) {
         return BigInt(
-            8406940278843123175743832327158906184461007565351952961240403202786066254437
+            "8406940278843123175743832327158906184461007565351952961240403202786066254437"
         );
     } else if (i == 10) {
         return BigInt(
-            18095170724440081982596227705073746612735437036256299083752673311093685115565
+            "18095170724440081982596227705073746612735437036256299083752673311093685115565"
         );
     } else if (i == 11) {
         return BigInt(
-            12494921685926131790120916419089462187828486801312615440551414562915682918055
+            "12494921685926131790120916419089462187828486801312615440551414562915682918055"
         );
     } else if (i == 12) {
         return BigInt(
-            10982166978621049276500916207228131554783228711662971846611531133859682131754
+            "10982166978621049276500916207228131554783228711662971846611531133859682131754"
         );
     } else if (i == 13) {
         return BigInt(
-            20706006017932272733008726576435564635242694158389807366729547655972642914556
+            "20706006017932272733008726576435564635242694158389807366729547655972642914556"
         );
     } else if (i == 14) {
         return BigInt(
-            12387411678914679768595095911604733054258673148265834445738162148229762835545
+            "12387411678914679768595095911604733054258673148265834445738162148229762835545"
         );
     } else if (i == 15) {
         return BigInt(
-            4645457020147420961926028699764295047993990626023916925718742337202088609186
+            "4645457020147420961926028699764295047993990626023916925718742337202088609186"
         );
     } else if (i == 16) {
         return BigInt(
-            20031172315068130033417068040619844856054469898708347831004393380441627976110
+            "20031172315068130033417068040619844856054469898708347831004393380441627976110"
         );
     } else if (i == 17) {
         return BigInt(
-            10706546104333770657460492059417658198254511559412115282441046503143202741181
+            "10706546104333770657460492059417658198254511559412115282441046503143202741181"
         );
     } else if (i == 18) {
         return BigInt(
-            11849011169801684430469139138252284578555021442080969739652766930478906494558
+            "11849011169801684430469139138252284578555021442080969739652766930478906494558"
         );
     } else if (i == 19) {
         return BigInt(
-            3314372264187560584702178823305973219176593083766293720214567837146802335937
+            "3314372264187560584702178823305973219176593083766293720214567837146802335937"
         );
     } else if (i == 20) {
         return BigInt(
-            1976389223243078417398278543048130277804852592158378794293220333410109017266
+            "1976389223243078417398278543048130277804852592158378794293220333410109017266"
         );
     } else {
         throw new Error("Index out of bounds");
     }
 }
+
+(async () => {
+    await initPoseidon(); // Make sure Poseidon is ready
+    const babyjub = await circomlibjs.buildBabyjub();
+    const F = babyjub.F;
+    const rawLeaves: bigint[] = [
+        BigInt("9544268642844573704226647894835027779097887973385979818262426097926729903982"),
+        BigInt("5581525453447415493315491882928005573898416079263491592050413242111209948978"),
+        BigInt("21879835473289089405366633954264833758077734612338952786509573456282948697519"),
+    ];
+
+    const [tree, levels] = buildMerkleTree(rawLeaves);
+    const root = tree[tree.length - 1][0];
+    console.log("Root:", root.toString());
+    console.log("Levels:", levels);
+
+    const index = 1;
+    const proof = getMerkleProof(tree, index);
+    console.log("Proof for leaf", index, ":", proof);
+
+
+    const circomProof = normalizeProof(proof, root, levels);
+
+    console.log("Circom proof format:");
+    console.log("leaf:", rawLeaves[index].toString());
+
+    console.log("final root:", circomProof.current.toString());
+    console.log("pathElements:", circomProof.pathElements);
+    console.log("pathIndices:", circomProof.pathIndices);
+
+
+
+})();
